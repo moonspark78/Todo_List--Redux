648ffbb7531fcb1c38250f378d83bf2e2534d95f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-23 15:55:14 +0100

    :wrench: init the project  :art:

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p className='text-7xl text-blue-600 text-center'>Hello Developper</p>
+    </>
+  )
+}
+
+export default App

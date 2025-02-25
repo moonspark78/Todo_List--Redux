cdd21bf45902b10aa528339b356b5621e501cac7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-25 16:10:07 +0100

    create the MARK_INCOMPLETED  action  :art:

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,7 @@ import { ADD_TODO } from "./actionTypes";
 import { TOGGLE_TODO } from "./actionTypes";
 import { REMOVE_TODO } from "./actionTypes";
 import { MARK_COMPLETED } from "./actionTypes";
+import { MARK_INCOMPLETED } from "./actionTypes";
 
 export const addTodo = (text) => ({
     type: ADD_TODO,
@@ -20,5 +21,10 @@ export const removeTodo = (id) => ({
 
 export const markCompleted = (id) => ({
     type: MARK_COMPLETED,
+    payload: {id}
+})
+
+export const markIncompleted = (id) => ({
+    type: MARK_INCOMPLETED,
     payload: {id}
 })

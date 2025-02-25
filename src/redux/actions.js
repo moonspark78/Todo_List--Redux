e7a73a62a8f682b308d2b591f13085606650d729
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-25 15:49:03 +0100

    create the toggleTodo  action  :art:

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,15 @@
 import { ADD_TODO } from "./actionTypes";
+import { TOGGLE_TODO } from "./actionTypes";
 import { REMOVE_TODO } from "./actionTypes";
 
 export const addTodo = (text) => ({
     type: ADD_TODO,
     payload: {text}
+})
+
+export const toggleTodo = (id) => ({
+    type:TOGGLE_TODO,
+    payload: {id}
 })
 
 export const removeTodo = (id) => ({

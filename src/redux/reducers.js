d25b46b5c537b99b39e29224eeb571628893e515
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-27 17:50:42 +0100

    create TOGGLE_TODO reducers  :art:

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { ADD_TODO } from "./actionTypes";
+import { ADD_TODO, TOGGLE_TODO } from "./actionTypes";
 
 const initialState = {
     todos: [],
@@ -11,7 +11,16 @@ const todoReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_TODO:
             return {
-                todos: [...state.todos, {text: action.payload.text, completed: true}]
+                todos: [...state.todos, {text: action.payload.text, completed: true}],
+                filter: state.filter,
+                searchTerm: state.searchTerm,
+            }
+        case TOGGLE_TODO:
+            return{
+                todos: state.todos.map((todo,index) => 
+                index === action.payload.id ? {...todo, completed: !todo.completed} : todo),
+                filter: state.filter,
+                searchTerm: state.searchTerm,
             }
         
             default:

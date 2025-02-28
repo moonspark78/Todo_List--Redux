8e88298520be188e1412157460f53a868868deb7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-02-28 14:54:20 +0100

    create the todo component  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import './App.css'
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import Todo from './components/Todo';
 
 function App() {
 
 
   return (
     <Provider store={store}>
-      <p className=' text-7xl text-blue-600 text-center'>Hello Developer</p>
+      <Todo/>
     </Provider>
   
   )

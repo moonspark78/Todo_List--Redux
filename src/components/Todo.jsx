26b1ba0ec895721befd6f9dbf901474df109f380
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-01 16:59:14 +0100

    working on the TODO component  :art:

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,9 @@
 
 const Todo = () => {
   return (
-    <div>Todo</div>
+    <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded-lg">
+      <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">PERSONNAL TODO</h2>
+    </div>
   )
 }
 

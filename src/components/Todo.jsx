2a4bce0082659cd84c38467c2ad1e88231beb080
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-02 14:35:38 +0100

    working on Todo component   :art:

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,14 @@
 import { BsPlus } from "react-icons/bs";
+import { useState } from "react";
 
 const Todo = () => {
+  const [newTodoText, setNewTodoText] = useState("");
+  console.log(newTodoText);
+
+  const handleAddTodo = () => {};
+  
+  const handleAddTodoClick = () => {};
+  
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded-lg">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">PERSONNAL TODO</h2>
@@ -8,6 +16,8 @@ const Todo = () => {
       {/* input text and btn */}
       <div className="flex items-center mb-4">
         <input
+          value={newTodoText}
+          onChange={(e) => setNewTodoText(e.target.value)}
           type="text"
           name="addTodoInput"
           id="addTodoInput"
@@ -15,7 +25,9 @@ const Todo = () => {
           className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
         />
         <button
-        className="ml-4 p-2 bg-blue-500 text-white hover:bg-blue-600 rounded">
+          className="ml-4 p-2 bg-blue-500 text-white hover:bg-blue-600 rounded"
+          onClick={handleAddTodoClick}
+        >
           <BsPlus />
         </button>
       </div>

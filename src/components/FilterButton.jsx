04248b5f97bb0582ffabadcf6f75a4bb2ed59500
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-03 14:57:44 +0100

    working on  the FilterButton component  :art:

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+
 
 const FilterButton = () => {
   return (
-    <div>FilterButton</div>
+    <div className="flex space-x-4 items-center">
+        <select>
+            <option value="ALL">Default</option>
+            <option value="COMPLETED">Completed</option>
+            <option value="INCOMPLETE">Incomplete</option>
+        </select>
+    </div>
   )
 }
 

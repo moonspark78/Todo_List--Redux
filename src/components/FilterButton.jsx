fb1bd256c6a711b9b52981987d6ddb70588b24bf
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-07 17:33:54 +0100

    working on the FilterButton component  :art:

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,9 +1,13 @@
-
+import { useDispatch, useSelector } from 'react-redux';
 
 const FilterButton = () => {
+  const dispatch = useDispatch();
+  const currentFilter = useSelector((state) => state.filter);
   return (
     <div className="flex space-x-4 items-center">
-        <select className="text-sm px-2 py-1 rounded border border-gray-300 focus:outline-none">
+        <select
+        value={currentFilter}
+         className="text-sm px-2 py-1 rounded border border-gray-300 focus:outline-none">
             <option value="ALL">Default</option>
             <option value="COMPLETED">Completed</option>
             <option value="INCOMPLETE">Incomplete</option>

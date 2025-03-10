b434c1f7482155ac6f55535855e6752e09dc0630
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-03-10 20:40:53 +0100

    🔥 working on  the TodoItem  :art:

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import {} from 'react-icons';
 import { FaToggleOff, FaToggleOn } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { toggleTodo } from '../redux/actions';
 
 const TodoItem = ({todo, index}) => {
+  const dispatch = useDispatch();
   return (
     <li className="flex flex-col sm:flex-row sm:items-center justify-between border-b-2 py-2 gap-4">
         <div className="flex items-center">
@@ -10,7 +13,7 @@ const TodoItem = ({todo, index}) => {
             <span className={`mr-4 ${todo.completed ? "line-through text-red-500" : " "}`}>{todo.text}</span>
         </div>
         <div className='space-x-3 ml-8'>
-            <button className='mr-2 text-sm bg-blue-500 text-white sm:px-2 py-1 rounded'>{todo.completed ? <FaToggleOff/> : <FaToggleOn/>}</button>
+            <button onClick={() => dispatch(toggleTodo())} className='mr-2 text-sm bg-blue-500 text-white sm:px-2 py-1 rounded'>{todo.completed ? <FaToggleOff/> : <FaToggleOn/>}</button>
         </div>
     </li>
   )
